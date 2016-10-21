8f01b1f390bea40e01b172a58637c73f92a644ba
gavs9 <gavs272@gmail.com>
2016-10-20 21:24:53 -0600

    Every 10 seconds

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-var job = new CronJob('0 */40 * * * *', function() {
+var job = new CronJob('*/10 * * * * *', function() {
     console.log('Init \n');
   }, function () {
     console.log('End \n');
