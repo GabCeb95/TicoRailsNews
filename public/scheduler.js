39e63ccd0c6ff6535054015c48a54db13f95365c
gavs9 <gavs272@gmail.com>
2016-10-20 22:07:26 -0600

    Job timer modified

--- public/scheduler.js
+++ public/scheduler.js
@@ -4,7 +4,7 @@ var rss = require('../routes/libs/rss');
 var ctrlNews = require('../controllers/news.controller.js');
 
 
-var job = new CronJob('0 */25 * * * *', function() {
+var job = new CronJob('0 */35 * * * *', function() {
     console.log('Init \n');
     rss.read('http://www.nacion.com/rss/latest/?contentType=NWS',function(result){
       try {
